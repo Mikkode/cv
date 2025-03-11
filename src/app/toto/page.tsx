e441baac10f0b990c825b105e49158252baccc47
Mickael <>
2025-03-12 00:41:13 +0900

    base 2

---
+++ src/app/toto/page.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { ShineBorder } from "@/components/magicui/shine-border";
+
+export default function ShineBorderDemo() {
+  return (
+    <Card className="relative overflow-hidden">
+      <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Enter your credentials to access your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid gap-4">
+            <div className="grid gap-2">a</div>
+            <div className="grid gap-2">a</div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Sign In</Button>
+      </CardFooter>
+    </Card>
+  );
+}
