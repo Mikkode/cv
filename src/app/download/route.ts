5ddc93c770bf2d6906ecd1d1d93aad666b60215a
Mickael <>
2025-03-14 12:32:29 +0900

    base 39

--- src/app/download/route.ts
+++ src/app/download/route.ts
@@ -2,14 +2,31 @@ import { NextResponse } from "next/server";
 import path from "path";
 import { promises as fs } from "fs";
 
-export async function GET() {
-  const filePath = path.resolve("public", "cv-mickael-warin.pdf");
-  const fileBuffer = await fs.readFile(filePath);
+export async function GET(request: Request) {
+  // Récupérer le paramètre 'locale' de l'URL
+  const { searchParams } = new URL(request.url);
+  const locale = searchParams.get("locale") || "fr";
 
-  return new NextResponse(fileBuffer, {
-    headers: {
-      "Content-Type": "application/pdf",
-      "Content-Disposition": 'attachment; filename="cv-mickael-warin.pdf"',
-    },
-  });
+  // Déterminer le nom du fichier en fonction de la langue
+  const filename =
+    locale === "en" ? "cv-mickael-warin-en.pdf" : "cv-mickael-warin-fr.pdf";
+
+  // Construire le chemin du fichier
+  const filePath = path.resolve("public", filename);
+
+  try {
+    // Lire le fichier
+    const fileBuffer = await fs.readFile(filePath);
+
+    // Retourner le fichier avec les en-têtes appropriés
+    return new NextResponse(fileBuffer, {
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename="${filename}"`,
+      },
+    });
+  } catch (error) {
+    // En cas d'erreur (fichier non trouvé, etc.)
+    return new NextResponse("FIle not found", { status: 404 });
+  }
 }
